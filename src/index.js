4688b22369a4c005937f8a486ab567bca2d2a1f0
Guilherme Camargo <guilhermecamargo57@hotmail.com>
2019-07-17 12:07:36 -0300

    second commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,11 @@ const main = () => {
     if (DESCRICAO != undefined && DESCRICAO != 'Descrição')
       return `INSERT INTO Products ( bar_code, name, price, quantity ) VALUES ('${CODE_BARRAS}', '${DESCRICAO.trim()}', ${VENDA.replace(',','.')}, ${parseInt(SALDO)});`
   })
-  fs.writeFileSync('./src/insert-products.sql', sql.join('\n'));
+  const products = readXLS('./src/products.xlsx').map( product =>{
+    if (product.DESCRICAO != undefined && product.DESCRICAO != 'Descrição')
+      return product
+  })
+  fs.writeFileSync('./src/products.js', JSON.stringify(products, null , 2));
 }
 
 main();
